782c6327d36fa969323cda38fb21673763757ca2
Luis Weich <weichluis.sw@gmail.com>
2021-05-20 19:10:18 +0200

    ewul-core add inject CoreModule

---
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/inject/CoreModule.java
@@ -0,0 +1,14 @@
+package org.ewul.core.inject;
+
+import com.google.inject.AbstractModule;
+
+public class CoreModule extends AbstractModule {
+
+    public CoreModule() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+}
