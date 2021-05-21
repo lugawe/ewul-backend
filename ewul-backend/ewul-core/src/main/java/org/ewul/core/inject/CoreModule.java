db98eba37d8b5704346d545c534fbf4cc5931dfa
Luis Weich <weichluis.sw@gmail.com>
2021-05-21 17:32:06 +0200

    CoreModule use config

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/inject/CoreModule.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/inject/CoreModule.java
@@ -1,14 +1,25 @@
 package org.ewul.core.inject;
 
 import com.google.inject.AbstractModule;
+import org.ewul.core.config.CoreConfiguration;
+import org.ewul.core.entity.EntityManagerHandler;
+
+import java.util.Objects;
 
 public class CoreModule extends AbstractModule {
 
-    public CoreModule() {
+    private final CoreConfiguration configuration;
+    private final EntityManagerHandler handler;
+
+    public CoreModule(CoreConfiguration configuration, EntityManagerHandler handler) {
+        this.configuration = Objects.requireNonNull(configuration);
+        this.handler = Objects.requireNonNull(handler);
     }
 
     @Override
     protected void configure() {
+        bind(CoreConfiguration.class).toInstance(configuration);
+        bind(EntityManagerHandler.class).toInstance(handler);
     }
 
 }
