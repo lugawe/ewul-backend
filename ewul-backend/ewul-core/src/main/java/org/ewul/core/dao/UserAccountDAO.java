672b83027dbc57a7e054b5be015f37a814864acf
Luis Weich <weichluis.sw@gmail.com>
2021-05-21 17:12:20 +0200

    ewul-core dao add UserAccountDAO

---
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/dao/UserAccountDAO.java
@@ -0,0 +1,25 @@
+package org.ewul.core.dao;
+
+import org.ewul.core.entity.EntityManagerHandler;
+import org.ewul.model.db.QUserAccount;
+import org.ewul.model.db.UserAccount;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.List;
+
+@Singleton
+public class UserAccountDAO extends BaseDAO<UserAccount> {
+
+    public static final QUserAccount Q_USER_ACCOUNT = new QUserAccount("userAccount");
+
+    @Inject
+    public UserAccountDAO(EntityManagerHandler handler) {
+        super(UserAccount.class, Q_USER_ACCOUNT, handler);
+    }
+
+    public List<UserAccount> getAll() {
+        return factory().selectFrom(Q_USER_ACCOUNT).fetch();
+    }
+
+}
