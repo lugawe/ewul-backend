27ef4790b42fdcc72661452c7b3c29bd302bbe13
Luis Weich <weichluis.sw@gmail.com>
2021-05-21 17:09:33 +0200

    ewul-core BaseDAO update

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/dao/BaseDAO.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/dao/BaseDAO.java
@@ -1,24 +1,34 @@
 package org.ewul.core.dao;
 
+import com.querydsl.core.types.EntityPath;
+import com.querydsl.jpa.impl.JPADeleteClause;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.jpa.impl.JPAUpdateClause;
 import org.ewul.core.entity.EntityManagerHandler;
 import org.ewul.model.db.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
 import java.util.Objects;
 
 public abstract class BaseDAO<T extends Model> {
 
+    private static final Logger log = LoggerFactory.getLogger(BaseDAO.class);
+
     protected final Class<T> entityClass;
+    protected final EntityPath<T> entityPath;
     protected final EntityManagerHandler handler;
 
-    protected BaseDAO(Class<T> entityClass, EntityManagerHandler handler) {
+    protected BaseDAO(Class<T> entityClass, EntityPath<T> entityPath, EntityManagerHandler handler) {
         this.entityClass = Objects.requireNonNull(entityClass);
+        this.entityPath = Objects.requireNonNull(entityPath);
         this.handler = Objects.requireNonNull(handler);
     }
 
-    protected EntityManager provide() {
+    protected final EntityManager provide() {
+        log.debug("provide");
         return handler.get();
     }
 
@@ -44,6 +54,14 @@ public abstract class BaseDAO<T extends Model> {
 
     public JPAQuery<?> query() {
         return query(0, 0);
+    }
+
+    public JPAUpdateClause update() {
+        return factory().update(entityPath);
+    }
+
+    public JPADeleteClause delete() {
+        return factory().delete(entityPath);
     }
 
     public final Class<T> getEntityClass() {
