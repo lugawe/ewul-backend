18a4fb9810987db456932c5db8d35c28156d29f0
Luis Weich <weichluis.sw@gmail.com>
2021-05-25 21:03:27 +0200

    BaseDAO remove EntityPath + factory design update

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/dao/BaseDAO.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/dao/BaseDAO.java
@@ -1,15 +1,13 @@
 package org.ewul.core.dao;
 
-import com.querydsl.core.types.EntityPath;
-import com.querydsl.jpa.impl.JPADeleteClause;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.querydsl.jpa.impl.JPAUpdateClause;
 import org.ewul.core.entity.EntityManagerHandler;
 import org.ewul.model.db.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Provider;
 import javax.persistence.EntityManager;
 import java.util.Objects;
 
@@ -18,12 +16,10 @@ public abstract class BaseDAO<T extends Model> {
     private static final Logger log = LoggerFactory.getLogger(BaseDAO.class);
 
     protected final Class<T> entityClass;
-    protected final EntityPath<T> entityPath;
     protected final EntityManagerHandler handler;
 
-    protected BaseDAO(Class<T> entityClass, EntityPath<T> entityPath, EntityManagerHandler handler) {
+    protected BaseDAO(Class<T> entityClass, EntityManagerHandler handler) {
         this.entityClass = Objects.requireNonNull(entityClass);
-        this.entityPath = Objects.requireNonNull(entityPath);
         this.handler = Objects.requireNonNull(handler);
     }
 
@@ -36,9 +32,12 @@ public abstract class BaseDAO<T extends Model> {
     }
 
     public JPAQueryFactory factory() {
-        EntityManager entityManager = provide();
-        configure(entityManager);
-        return new JPAQueryFactory(entityManager);
+        Provider<EntityManager> provider = () -> {
+            EntityManager entityManager = provide();
+            configure(entityManager);
+            return entityManager;
+        };
+        return new JPAQueryFactory(provider);
     }
 
     public JPAQuery<?> query(long offset, long limit) {
@@ -54,14 +53,6 @@ public abstract class BaseDAO<T extends Model> {
 
     public JPAQuery<?> query() {
         return query(0, 0);
-    }
-
-    public JPAUpdateClause update() {
-        return factory().update(entityPath);
-    }
-
-    public JPADeleteClause delete() {
-        return factory().delete(entityPath);
     }
 
     public final Class<T> getEntityClass() {
