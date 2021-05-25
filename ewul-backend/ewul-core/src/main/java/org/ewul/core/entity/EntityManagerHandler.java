b99f0848605b6b3931b0cf2488e7b0e3044ab211
Luis Weich <weichluis.sw@gmail.com>
2021-05-25 20:53:16 +0200

    EntityManagerHandler add Closeable

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/entity/EntityManagerHandler.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/entity/EntityManagerHandler.java
@@ -3,11 +3,12 @@ package org.ewul.core.entity;
 import javax.inject.Provider;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.io.Closeable;
 
-public interface EntityManagerHandler extends Provider<EntityManager> {
-
-    String getName();
+public interface EntityManagerHandler extends Provider<EntityManager>, Closeable {
 
     EntityManagerFactory getFactory();
+
+    String getName();
 
 }
