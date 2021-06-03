2bbc37b7009ee41ddedfe2eec6368298e1719420
Luis Weich <weichluis.sw@gmail.com>
2021-06-03 15:34:19 +0200

    ewul-core Ewul create injector

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/Ewul.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/Ewul.java
@@ -1,14 +1,33 @@
 package org.ewul.core;
 
+import com.google.common.collect.ObjectArrays;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import org.ewul.core.config.CoreConfiguration;
+import org.ewul.core.entity.EntityManagerHandler;
+import org.ewul.core.inject.CoreModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Ewul {
+public final class Ewul {
 
     private static final Logger log = LoggerFactory.getLogger(Ewul.class);
 
+    private Ewul() {
+    }
+
     public static void init() {
-        log.info("ewul - init");
+        log.info("init");
+    }
+
+    public static Injector injector(CoreConfiguration configuration,
+                                    EntityManagerHandler handler,
+                                    Module... modules) {
+        log.info("create injector");
+
+        Module coreModule = new CoreModule(configuration, handler);
+        return Guice.createInjector(ObjectArrays.concat(coreModule, modules));
     }
 
 }
