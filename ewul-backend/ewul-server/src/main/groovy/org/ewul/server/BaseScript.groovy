6f2026915d179d5dca48df467ac1cc9e99f8cc34
Luis Weich <weichluis.sw@gmail.com>
2021-06-08 20:22:13 +0200

    server security config update

--- ewul-backend/ewul-server/src/main/groovy/org/ewul/server/BaseScript.groovy
+++ ewul-backend/ewul-server/src/main/groovy/org/ewul/server/BaseScript.groovy
@@ -5,12 +5,16 @@ import groovy.util.logging.Slf4j
 @Slf4j
 abstract class BaseScript<T> extends Script {
 
+    protected BaseScript() {
+        log.debug("new script created")
+    }
+
     @Override
     Object run() {
         log.debug("calling execute")
         return execute()
     }
 
-    abstract T execute()
+    abstract T execute() throws Exception
 
 }
--- ewul-backend/ewul-server/src/main/java/org/ewul/server/security/SecurityConfig.java
+++ ewul-backend/ewul-server/src/main/java/org/ewul/server/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package org.ewul.server.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,7 +13,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
     public SecurityConfig() {
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder builder) throws Exception {
     }
 
     @Override
