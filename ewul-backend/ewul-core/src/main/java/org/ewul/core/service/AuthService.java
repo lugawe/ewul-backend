30bf38b595697c3be34a589450c6f838bebbfb2a
Luis Weich <weichluis.sw@gmail.com>
2021-06-09 18:58:43 +0200

    ewul-core add AuthService

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/service/AuthService.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/service/AuthService.java
@@ -2,19 +2,23 @@ package org.ewul.core.service;
 
 import org.ewul.core.config.CoreConfiguration;
 import org.ewul.core.dao.UserAccountDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.Objects;
 
 @Singleton
-public class UserAccountService {
+public class AuthService {
 
-    private final CoreConfiguration configuration;
-    private final UserAccountDAO userAccountDAO;
+    private static final Logger log = LoggerFactory.getLogger(AuthService.class);
+
+    protected final CoreConfiguration configuration;
+    protected final UserAccountDAO userAccountDAO;
 
     @Inject
-    public UserAccountService(CoreConfiguration configuration, UserAccountDAO userAccountDAO) {
+    public AuthService(CoreConfiguration configuration, UserAccountDAO userAccountDAO) {
         this.configuration = Objects.requireNonNull(configuration);
         this.userAccountDAO = Objects.requireNonNull(userAccountDAO);
     }
