9fec9424a8c4e60cd5deffdea0a193a1b743ba6c
Luis Weich <weichluis.sw@gmail.com>
2021-06-26 22:41:58 +0200

    UserAccountDAO getById

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/dao/UserAccountDAO.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/dao/UserAccountDAO.java
@@ -1,12 +1,14 @@
 package org.ewul.core.dao;
 
-import org.ewul.core.entity.EntityManagerHandler;
+import org.ewul.core.entity.TransactionHandler;
 import org.ewul.model.db.QUserAccount;
 import org.ewul.model.db.UserAccount;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Singleton
 public class UserAccountDAO extends BaseDAO<UserAccount> {
@@ -14,8 +16,16 @@ public class UserAccountDAO extends BaseDAO<UserAccount> {
     public static final QUserAccount Q_USER_ACCOUNT = new QUserAccount("userAccount");
 
     @Inject
-    public UserAccountDAO(EntityManagerHandler handler) {
+    public UserAccountDAO(TransactionHandler handler) {
         super(UserAccount.class, handler);
+    }
+
+    public Optional<UserAccount> getById(UUID id) {
+        if (id == null) {
+            throw new NullPointerException("param id");
+        }
+        UserAccount account = factory().selectFrom(Q_USER_ACCOUNT).where(Q_USER_ACCOUNT.id.eq(id)).fetchOne();
+        return Optional.ofNullable(account);
     }
 
     public List<UserAccount> getAll() {
