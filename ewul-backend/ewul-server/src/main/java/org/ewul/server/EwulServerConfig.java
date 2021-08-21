2a51a78a4c7cb33cdd3361052b9a975535af3688
Luis Weich <weichluis.sw@gmail.com>
2021-08-21 18:01:55 +0200

    update EwulServerConfig

--- ewul-backend/ewul-server/src/main/java/org/ewul/server/EwulServerConfig.java
+++ ewul-backend/ewul-server/src/main/java/org/ewul/server/EwulServerConfig.java
@@ -1,44 +1,24 @@
 package org.ewul.server;
 
-import com.google.inject.Injector;
-import org.ewul.core.Ewul;
 import org.ewul.core.config.CoreConfiguration;
-import org.ewul.core.entity.TransactionHandler;
-import org.ewul.core.service.AuthService;
-import org.ewul.server.hibernate.HibernateHandler;
-import org.hibernate.SessionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import javax.inject.Inject;
-
 @Configuration
+@ConfigurationProperties
 public class EwulServerConfig {
 
-    private static final Logger log = LoggerFactory.getLogger(EwulServerConfig.class);
+    private CoreConfiguration coreConfiguration = new CoreConfiguration();
 
-    private final Injector injector;
-
-    @Inject
-    public EwulServerConfig(SessionFactory sessionFactory) {
-        this.injector = Ewul.injector(CoreConfiguration.empty(), new HibernateHandler(sessionFactory));
+    public EwulServerConfig() {
     }
 
-    @Bean
-    public Injector injector() {
-        return injector;
+    public CoreConfiguration getCoreConfiguration() {
+        return coreConfiguration;
     }
 
-    @Bean
-    public TransactionHandler transactionHandler() {
-        return injector.getInstance(TransactionHandler.class);
-    }
-
-    @Bean
-    public AuthService authService() {
-        return injector.getInstance(AuthService.class);
+    public void setCoreConfiguration(CoreConfiguration coreConfiguration) {
+        this.coreConfiguration = coreConfiguration;
     }
 
 }
