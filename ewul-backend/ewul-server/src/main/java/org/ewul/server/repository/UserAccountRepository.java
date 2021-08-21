c261d76a95d57dfe873d76d8045759106fc43c40
Luis Weich <weichluis.sw@gmail.com>
2021-08-21 17:45:12 +0200

    remove UserAccountRepository

--- ewul-backend/ewul-server/src/main/java/org/ewul/server/repository/UserAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ewul.server.repository;
-
-import org.ewul.model.db.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, UUID> {
-}
