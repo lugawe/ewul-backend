de3151a499e67d67b371fb93727cf1df5f686ef6
Luis Weich <weichluis.sw@gmail.com>
2021-08-23 21:21:10 +0200

    AuthService login

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/service/AuthService.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/service/AuthService.java
@@ -1,13 +1,18 @@
 package org.ewul.core.service;
 
 import org.ewul.core.config.CoreConfiguration;
+import org.ewul.core.dao.PasswordDAO;
 import org.ewul.core.dao.UserAccountDAO;
+import org.ewul.model.db.Password;
+import org.ewul.model.db.UserAccount;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 @Singleton
 public class AuthService {
@@ -16,11 +21,39 @@ public class AuthService {
 
     protected final CoreConfiguration configuration;
     protected final UserAccountDAO userAccountDAO;
+    protected final PasswordDAO passwordDAO;
 
     @Inject
-    public AuthService(CoreConfiguration configuration, UserAccountDAO userAccountDAO) {
+    public AuthService(CoreConfiguration configuration,
+                       UserAccountDAO userAccountDAO,
+                       PasswordDAO passwordDAO) {
+
         this.configuration = Objects.requireNonNull(configuration);
         this.userAccountDAO = Objects.requireNonNull(userAccountDAO);
+        this.passwordDAO = Objects.requireNonNull(passwordDAO);
+    }
+
+    protected boolean checkPassword(String plain, Password password) {
+        // TODO implementation
+        return true;
+    }
+
+    public Optional<UserAccount> login(String name, String password, Predicate<UserAccount> filter) {
+
+        Optional<UserAccount> _account = userAccountDAO.getByName(name);
+        if (_account.isPresent()) {
+            UserAccount account = _account.get();
+            if (filter.test(account) && checkPassword(password, account.getPassword())) {
+                return Optional.of(account);
+            }
+        }
+
+        log.warn("invalid login attempt: {}", name);
+        return Optional.empty();
+    }
+
+    public Optional<UserAccount> login(String name, String password) {
+        return login(name, password, Objects::nonNull);
     }
 
 }
