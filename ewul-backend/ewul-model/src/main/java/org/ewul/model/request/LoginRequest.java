e6ae6bab6a1384b70f164e0c75bca0a013512af1
Luis Weich <weichluis.sw@gmail.com>
2021-09-18 14:01:22 +0200

    add LoginRequest

---
+++ ewul-backend/ewul-model/src/main/java/org/ewul/model/request/LoginRequest.java
@@ -0,0 +1,36 @@
+package org.ewul.model.request;
+
+public class LoginRequest {
+
+    private String hash;
+    private String email;
+    private String password;
+
+    public LoginRequest() {
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
