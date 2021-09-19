bfb4210e80db937496cf5267df5b5a2903a871db
Luis Weich <weichluis.sw@gmail.com>
2021-09-19 17:09:32 +0200

    auth updates

--- ewul-backend/ewul-server/src/main/java/org/ewul/server/auth/UserAuthenticator.java
+++ ewul-backend/ewul-server/src/main/java/org/ewul/server/auth/UserAuthenticator.java
@@ -2,12 +2,14 @@ package org.ewul.server.auth;
 
 import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.Authenticator;
+import org.ewul.core.jwt.JwtHandler;
 import org.ewul.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.Objects;
 import java.util.Optional;
 
 @Singleton
@@ -15,13 +17,29 @@ public class UserAuthenticator implements Authenticator<String, User> {
 
     private static final Logger log = LoggerFactory.getLogger(UserAuthenticator.class);
 
+    protected final JwtHandler jwtHandler;
+
     @Inject
-    public UserAuthenticator() {
+    public UserAuthenticator(JwtHandler jwtHandler) {
+        this.jwtHandler = Objects.requireNonNull(jwtHandler);
     }
 
     @Override
     public Optional<User> authenticate(String token) throws AuthenticationException {
-        return Optional.empty();
+        try {
+            User user = jwtHandler.decode(token);
+            if (user == null || user.getId() == null) {
+                throw new IllegalStateException("invalid jwt token");
+            }
+            log.info("user has successfully authenticated: {}", user);
+            return Optional.of(user);
+        } catch (Exception ignored) {
+            return Optional.empty();
+        }
+    }
+
+    public JwtHandler getJwtHandler() {
+        return jwtHandler;
     }
 
 }
--- ewul-backend/ewul-server/src/main/java/org/ewul/server/auth/UserAuthorizer.java
+++ ewul-backend/ewul-server/src/main/java/org/ewul/server/auth/UserAuthorizer.java
@@ -2,26 +2,31 @@ package org.ewul.server.auth;
 
 import io.dropwizard.auth.Authorizer;
 import org.ewul.model.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.container.ContainerRequestContext;
+import java.util.Collection;
+import java.util.Collections;
 
 @Singleton
 public class UserAuthorizer implements Authorizer<User> {
-
-    private static final Logger log = LoggerFactory.getLogger(UserAuthorizer.class);
 
     @Inject
     public UserAuthorizer() {
     }
 
+    protected Collection<String> emptyIfNull(Collection<String> collection) {
+        return collection != null ? collection : Collections.emptySet();
+    }
+
     @Override
     public boolean authorize(User user, String role, @Nullable ContainerRequestContext context) {
-        return false;
+        if (user == null || role == null) {
+            return false;
+        }
+        return emptyIfNull(user.getRoles()).stream().anyMatch(r -> r.equals(role));
     }
 
     @Override
