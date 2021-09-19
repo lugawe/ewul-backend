b1e94ddebc48eb17826bde9460f6eda197a01b9d
Luis Weich <weichluis.sw@gmail.com>
2021-09-19 19:31:56 +0200

    CoreConfiguration valid jwtConfiguration

--- ewul-backend/ewul-model/src/main/java/org/ewul/model/config/CoreConfiguration.java
+++ ewul-backend/ewul-model/src/main/java/org/ewul/model/config/CoreConfiguration.java
@@ -2,12 +2,16 @@ package org.ewul.model.config;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 public class CoreConfiguration implements Serializable {
 
     private static final CoreConfiguration EMPTY = new CoreConfiguration();
 
+    @Valid
+    @NotNull
     private JwtConfiguration jwtConfiguration = new JwtConfiguration();
 
     public CoreConfiguration() {
