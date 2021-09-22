3cc1b83775d2d13cb34b495ab2dc72532bc76cb6
Luis Weich <weichluis.sw@gmail.com>
2021-09-22 21:01:20 +0200

    Roles add visitor

--- ewul-backend/ewul-model/src/main/java/org/ewul/model/Roles.java
+++ ewul-backend/ewul-model/src/main/java/org/ewul/model/Roles.java
@@ -3,6 +3,8 @@ package org.ewul.model;
 public final class Roles {
 
     public static final String ADMIN = "ADMIN";
+    public static final String VISITOR = "VISITOR";
+
     public static final String READ = "READ";
     public static final String WRITE = "WRITE";
 
