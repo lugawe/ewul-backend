051f524b10495da490d82559589b863592b7b087
Luis Weich <weichluis.sw@gmail.com>
2021-10-04 22:09:42 +0200

    JwtHandler remove generateVisitorJwt

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/service/AuthService.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/service/AuthService.java
@@ -80,8 +80,4 @@ public class AuthService {
         return jwtHandler.generateJwt(account);
     }
 
-    public String generateVisitorJwt(String name, String ip) {
-        return jwtHandler.generateVisitorJwt(name, ip);
-    }
-
 }
