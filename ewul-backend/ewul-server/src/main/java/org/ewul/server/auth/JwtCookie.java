e203d2b33c841210b64038d0f2e0bd9182c5d4e2
Luis Weich <weichluis.sw@gmail.com>
2021-10-21 22:00:43 +0200

    create JwtCookie helper class

---
+++ ewul-backend/ewul-server/src/main/java/org/ewul/server/auth/JwtCookie.java
@@ -0,0 +1,46 @@
+package org.ewul.server.auth;
+
+import org.ewul.server.util.CookieBuilder;
+
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.NewCookie;
+import java.time.Duration;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public final class JwtCookie {
+
+    public static final String AUTH_COOKIE_NAME = "auth_jwt";
+
+    private JwtCookie() {
+    }
+
+    public static String extractCookie(Map<String, Cookie> cookies) throws IllegalArgumentException {
+
+        if (cookies == null || cookies.size() < 1) {
+            throw new IllegalArgumentException("no cookies provided");
+        }
+
+        Optional<Cookie> cookie = Optional.ofNullable(cookies.get(AUTH_COOKIE_NAME));
+        if (!cookie.isPresent()) {
+            throw new IllegalArgumentException(String.format("no %s cookie provided", AUTH_COOKIE_NAME));
+        }
+
+        return cookie.get().getValue();
+    }
+
+    public static NewCookie createDefaultCookie(String jwt, Duration lifetime) {
+        Objects.requireNonNull(jwt);
+        Objects.requireNonNull(lifetime);
+        int maxAge = (int) (lifetime.toMillis() / 1000);
+        return new CookieBuilder()
+                .withName(AUTH_COOKIE_NAME)
+                .withValue(jwt)
+                .withMaxAge(maxAge)
+                .withHttpOnly(true)
+                .withPath("/api/")
+                .buildNewCookie();
+    }
+
+}
--- ewul-backend/ewul-server/src/main/java/org/ewul/server/auth/JwtCookieAuthFilter.java
+++ ewul-backend/ewul-server/src/main/java/org/ewul/server/auth/JwtCookieAuthFilter.java
@@ -2,25 +2,16 @@ package org.ewul.server.auth;
 
 import io.dropwizard.auth.AuthFilter;
 import org.ewul.model.User;
-import org.ewul.server.util.CookieBuilder;
 
 import javax.annotation.Priority;
 import javax.inject.Inject;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.core.Cookie;
-import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.SecurityContext;
-import java.time.Duration;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 
 @Priority(Priorities.AUTHENTICATION)
 public class JwtCookieAuthFilter extends AuthFilter<String, User> {
-
-    public static final String AUTH_COOKIE_NAME = "auth_jwt";
 
     public static class Builder extends AuthFilterBuilder<String, User, JwtCookieAuthFilter> {
 
@@ -44,17 +35,9 @@ public class JwtCookieAuthFilter extends AuthFilter<String, User> {
 
         try {
 
-            Map<String, Cookie> cookies = context.getCookies();
-            if (cookies == null || cookies.size() < 1) {
-                throw new IllegalStateException("no cookies provided");
-            }
+            String value = JwtCookie.extractCookie(context.getCookies());
 
-            Optional<Cookie> cookie = Optional.ofNullable(cookies.get(AUTH_COOKIE_NAME));
-            if (!cookie.isPresent()) {
-                throw new IllegalStateException(String.format("no %s cookie provided", AUTH_COOKIE_NAME));
-            }
-
-            if (!authenticate(context, cookie.get().getValue(), SecurityContext.BASIC_AUTH)) {
+            if (value == null || !authenticate(context, value, SecurityContext.BASIC_AUTH)) {
                 throw new IllegalStateException("invalid jwt token");
             }
 
@@ -62,19 +45,6 @@ public class JwtCookieAuthFilter extends AuthFilter<String, User> {
             throw new WebApplicationException(ex, unauthorizedHandler.buildResponse(prefix, realm));
         }
 
-    }
-
-    public static NewCookie createDefaultCookie(String jwt, Duration lifetime) {
-        Objects.requireNonNull(jwt);
-        Objects.requireNonNull(lifetime);
-        int maxAge = (int) (lifetime.toMillis() / 1000);
-        return new CookieBuilder()
-                .withName(AUTH_COOKIE_NAME)
-                .withValue(jwt)
-                .withMaxAge(maxAge)
-                .withHttpOnly(true)
-                .withPath("/api/")
-                .buildNewCookie();
     }
 
 }
