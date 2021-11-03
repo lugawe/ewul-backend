e4c7a81c7fb2f4c8948ee667fb9ea7ad4d2ac5eb
Luis Weich <weichluis.sw@gmail.com>
2021-11-03 21:43:58 +0100

    add SetUtils

---
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/util/SetUtils.java
@@ -0,0 +1,32 @@
+package org.ewul.core.util;
+
+import java.util.*;
+
+public final class SetUtils {
+
+    private SetUtils() {
+    }
+
+    public static <T> SortedSet<T> sortedSet(Comparator<T> comparator, Collection<? extends T> values) {
+        if (comparator == null) {
+            throw new NullPointerException("param comparator");
+        }
+        if (values == null || values.isEmpty()) {
+            values = Collections.emptySet();
+        }
+        SortedSet<T> sortedSet = new TreeSet<>(comparator);
+        sortedSet.addAll(values);
+        return sortedSet;
+    }
+
+    @SafeVarargs
+    public static <T> SortedSet<T> sortedSet(Comparator<T> comparator, T... values) {
+        return sortedSet(comparator, Arrays.asList(values));
+    }
+
+    @SafeVarargs
+    public static <T extends Comparable<T>> SortedSet<T> sortedSet(T... values) {
+        return sortedSet(Comparator.naturalOrder(), Arrays.asList(values));
+    }
+
+}
