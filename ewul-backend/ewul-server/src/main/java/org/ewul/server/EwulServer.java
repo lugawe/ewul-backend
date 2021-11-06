91ab192b5ec03282d08bdff6b027fcc7eba8234d
Luis Weich <weichluis.sw@gmail.com>
2021-11-06 12:35:49 +0100

    EwulServer updates + version log

--- ewul-backend/ewul-server/src/main/java/org/ewul/server/EwulServer.java
+++ ewul-backend/ewul-server/src/main/java/org/ewul/server/EwulServer.java
@@ -1,20 +1,18 @@
 package org.ewul.server;
 
-import com.google.common.eventbus.EventBus;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.ewul.core.util.Lazy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.vyarus.dropwizard.guice.GuiceBundle;
 
 public class EwulServer extends Application<EwulServerConfig> {
 
-    public static final Lazy<EwulServer> INSTANCE = Lazy.of(EwulServer::new);
-    public static final Lazy<EventBus> EVENT_BUS = Lazy.of(() -> new EventBus("ewul-server-main"));
+    private static final Logger log = LoggerFactory.getLogger(EwulServer.class);
 
-    public static EventBus events() {
-        return EVENT_BUS.get();
-    }
+    public static final Lazy<EwulServer> INSTANCE = Lazy.of(EwulServer::new);
 
     public static void main(String[] args) throws Exception {
         INSTANCE.get().run(args);
@@ -31,6 +29,7 @@ public class EwulServer extends Application<EwulServerConfig> {
 
     @Override
     public void run(EwulServerConfig config, Environment environment) {
+        log.info("running version: {}", Constants.VERSION);
         environment.jersey().setUrlPattern("/api/*");
     }
 
