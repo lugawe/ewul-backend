530734c9d47159283eb5df82d2fe79f865f255ce
Luis Weich <weichluis.sw@gmail.com>
2021-11-06 12:42:24 +0100

    database entities getting scanned

--- ewul-backend/ewul-server/src/main/java/org/ewul/server/EwulServerModule.java
+++ ewul-backend/ewul-server/src/main/java/org/ewul/server/EwulServerModule.java
@@ -1,32 +1,20 @@
 package org.ewul.server;
 
 import io.dropwizard.db.PooledDataSourceFactory;
-import io.dropwizard.hibernate.HibernateBundle;
+import io.dropwizard.hibernate.ScanningHibernateBundle;
 import org.ewul.core.entity.EntityDataHandler;
 import org.ewul.core.util.Lazy;
 import org.ewul.model.config.CoreConfiguration;
-import org.ewul.model.db.DbModel;
-import org.ewul.model.db.auth.Account;
-import org.ewul.model.db.auth.Group;
-import org.ewul.model.db.auth.Password;
-import org.ewul.model.db.auth.Role;
 import org.ewul.server.hibernate.HibernateHandler;
 import org.hibernate.SessionFactory;
 import ru.vyarus.dropwizard.guice.module.support.DropwizardAwareModule;
 
 public class EwulServerModule extends DropwizardAwareModule<EwulServerConfig> {
 
-    public static class EwulHibernateBundle extends HibernateBundle<EwulServerConfig> {
-
-        public static final Class<?>[] ENTITIES = {
-                Account.class,
-                Group.class,
-                Password.class,
-                Role.class
-        };
+    public static class EwulHibernateBundle extends ScanningHibernateBundle<EwulServerConfig> {
 
         public EwulHibernateBundle() {
-            super(DbModel.class, ENTITIES);
+            super("org.ewul.model.db");
         }
 
         @Override
