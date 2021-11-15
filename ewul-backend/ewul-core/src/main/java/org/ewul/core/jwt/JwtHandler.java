fd464ef08dec5ad7d8670280b4b6303c44237bae
Luis Weich <weichluis.sw@gmail.com>
2021-11-15 21:43:40 +0100

    JwtHandler update

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/jwt/JwtHandler.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/jwt/JwtHandler.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.JWTVerifier;
 import com.auth0.jwt.interfaces.Verification;
 import org.ewul.core.util.MapUtils;
 import org.ewul.model.BasicUser;
@@ -12,7 +11,6 @@ import org.ewul.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
@@ -202,7 +200,6 @@ public class JwtHandler {
 
     protected final Algorithm algorithm;
 
-    @Inject
     public JwtHandler(Algorithm algorithm) {
         this.algorithm = Objects.requireNonNull(algorithm);
     }
