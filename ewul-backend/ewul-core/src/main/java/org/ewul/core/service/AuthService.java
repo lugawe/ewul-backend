af8679f2503042d65b086f6a76fef7900f015102
Luis Weich <weichluis.sw@gmail.com>
2021-12-22 20:11:42 +0100

    AuthService update

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/service/AuthService.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/service/AuthService.java
@@ -3,7 +3,8 @@ package org.ewul.core.service;
 import org.ewul.core.dao.auth.AccountDAO;
 import org.ewul.core.dao.auth.GroupDAO;
 import org.ewul.core.dao.auth.PasswordDAO;
-import org.ewul.core.jwt.AccountJwtHandler;
+import org.ewul.core.dao.auth.RoleDAO;
+import org.ewul.core.modules.auth.PasswordHashing;
 import org.ewul.model.config.CoreConfiguration;
 import org.ewul.model.db.auth.Account;
 import org.ewul.model.db.auth.Group;
@@ -24,35 +25,40 @@ public class AuthService {
     private static final Logger log = LoggerFactory.getLogger(AuthService.class);
 
     protected final CoreConfiguration configuration;
+
     protected final AccountDAO accountDAO;
     protected final GroupDAO groupDAO;
     protected final PasswordDAO passwordDAO;
-    protected final AccountJwtHandler accountJwtHandler;
+    protected final RoleDAO roleDAO;
+
+    protected final PasswordHashing passwordHashing;
 
     @Inject
     public AuthService(CoreConfiguration configuration,
                        AccountDAO accountDAO,
                        GroupDAO groupDAO,
                        PasswordDAO passwordDAO,
-                       AccountJwtHandler accountJwtHandler) {
+                       RoleDAO roleDAO,
+                       PasswordHashing passwordHashing) {
 
         this.configuration = Objects.requireNonNull(configuration);
         this.accountDAO = Objects.requireNonNull(accountDAO);
         this.groupDAO = Objects.requireNonNull(groupDAO);
         this.passwordDAO = Objects.requireNonNull(passwordDAO);
-        this.accountJwtHandler = Objects.requireNonNull(accountJwtHandler);
+        this.roleDAO = Objects.requireNonNull(roleDAO);
+        this.passwordHashing = Objects.requireNonNull(passwordHashing);
     }
 
     public Account register(String email, String name, String plainPassword) {
 
         if (email == null || email.isEmpty()) {
-            throw new IllegalArgumentException("param email");
+            throw new IllegalArgumentException("email");
         }
         if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("param name");
+            throw new IllegalArgumentException("name");
         }
         if (plainPassword == null || plainPassword.isEmpty()) {
-            throw new IllegalArgumentException("param plainPassword");
+            throw new IllegalArgumentException("plainPassword");
         }
 
         if (accountDAO.checkEmailExists(email)) {
@@ -73,7 +79,8 @@ public class AuthService {
         Group group = groupDAO.getDefaultGroup();
         account.setGroup(group);
 
-        Password password = passwordDAO.createPassword(plainPassword);
+        String hashedPassword = passwordHashing.hash(plainPassword);
+        Password password = passwordDAO.createPassword(hashedPassword);
         account.setPassword(password);
 
         if (accountDAO.factory().insert(account) == null) {
@@ -87,24 +94,27 @@ public class AuthService {
         account.setLastAccess(LocalDateTime.now());
     }
 
-    public Optional<Account> login(String email, String password, Predicate<Account> filter) {
+    public Optional<Account> login(String email, String plainPassword, Predicate<Account> filter) {
 
         if (email == null || email.isEmpty()) {
-            throw new IllegalArgumentException("param email");
+            throw new IllegalArgumentException("email");
         }
-        if (password == null || password.isEmpty()) {
-            throw new IllegalArgumentException("param password");
+        if (plainPassword == null || plainPassword.isEmpty()) {
+            throw new IllegalArgumentException("password");
         }
         if (filter == null) {
-            throw new IllegalArgumentException("param filter");
+            throw new IllegalArgumentException("filter");
         }
 
         Optional<Account> _account = accountDAO.getByEmail(email);
         if (_account.isPresent()) {
             Account account = _account.get();
-            if (filter.test(account) && passwordDAO.checkPassword(password, account.getPassword())) {
-                update(account);
-                return Optional.of(account);
+            if (filter.test(account)) {
+                Password password = account.getPassword();
+                if (password != null && passwordHashing.check(plainPassword, password.getHash())) {
+                    update(account);
+                    return Optional.of(account);
+                }
             }
             log.info("invalid login attempt: {}", email);
         }
@@ -114,10 +124,6 @@ public class AuthService {
 
     public Optional<Account> login(String email, String password) {
         return login(email, password, Objects::nonNull);
-    }
-
-    public String generateJwt(Account account) {
-        return accountJwtHandler.generateAccountJwt(account);
     }
 
 }
