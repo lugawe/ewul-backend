29a66c760d7b9dbe4c5c4699e467d2a4bccb106d
Luis Weich <weichluis.sw@gmail.com>
2021-12-22 19:57:21 +0100

    update PasswordDAO

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/dao/auth/PasswordDAO.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/dao/auth/PasswordDAO.java
@@ -2,53 +2,36 @@ package org.ewul.core.dao.auth;
 
 import org.ewul.core.dao.BaseDAO;
 import org.ewul.core.entity.EntityDataHandler;
-import org.ewul.core.security.hashing.PasswordHashing;
 import org.ewul.model.db.auth.Password;
 import org.ewul.model.db.auth.QPassword;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.Objects;
 
 @Singleton
 public class PasswordDAO extends BaseDAO<Password> {
 
     private static final QPassword PATH = new QPassword("auth_password");
 
-    protected final PasswordHashing passwordHashing;
-
     @Inject
-    public PasswordDAO(EntityDataHandler handler, PasswordHashing passwordHashing) {
+    public PasswordDAO(EntityDataHandler handler) {
         super(Password.class, handler);
-        this.passwordHashing = Objects.requireNonNull(passwordHashing);
     }
 
-    public Password createPassword(String plainPassword) {
+    public Password createPassword(String hashedPassword) {
 
-        if (plainPassword == null) {
-            throw new NullPointerException("plainPassword");
+        if (hashedPassword == null) {
+            throw new NullPointerException("hashedPassword");
         }
 
         Password password = new Password();
-        password.setHash(passwordHashing.hash(plainPassword));
+        password.setHash(hashedPassword);
 
         if (factory().insert(password) == null) {
             throw new IllegalStateException("cannot insert password entity");
         }
 
         return password;
-    }
-
-    public boolean checkPassword(String plainPassword, Password password) {
-
-        if (plainPassword == null) {
-            throw new NullPointerException("plainPassword");
-        }
-        if (password == null) {
-            throw new NullPointerException("password");
-        }
-
-        return passwordHashing.check(plainPassword, password.getHash());
     }
 
 }
