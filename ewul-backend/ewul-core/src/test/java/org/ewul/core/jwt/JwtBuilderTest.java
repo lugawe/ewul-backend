84492f0d6c97f62b0ae5a4cd35dd67a116f02504
Luis Weich <weichluis.sw@gmail.com>
2022-01-28 23:49:40 +0100

    add JwtBuilderTest

---
+++ ewul-backend/ewul-core/src/test/java/org/ewul/core/jwt/JwtBuilderTest.java
@@ -0,0 +1,46 @@
+package org.ewul.core.jwt;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.google.common.collect.Sets;
+import org.ewul.model.BasicUser;
+import org.ewul.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class JwtBuilderTest {
+
+    static JwtBuilder jwtBuilder1;
+    static JwtBuilder jwtBuilder2;
+
+    @BeforeAll
+    static void init() {
+        jwtBuilder1 = new JwtBuilder(Algorithm.HMAC256("foo"));
+        jwtBuilder2 = new JwtBuilder(Algorithm.HMAC256("bar"));
+    }
+
+    @Test
+    void encode_test() {
+
+        User user = new BasicUser() {{
+            setId(UUID.randomUUID());
+            setName("Test-User");
+            setRoles(Sets.newHashSet("1", "2"));
+            setProperties(Collections.singletonMap("a", "b"));
+        }};
+
+        String accessToken1 = jwtBuilder1.buildAccessToken(user);
+
+        assertNotNull(accessToken1);
+
+        String refreshToken1 = jwtBuilder1.buildRefreshToken(user);
+
+        assertNotNull(refreshToken1);
+
+    }
+
+}
