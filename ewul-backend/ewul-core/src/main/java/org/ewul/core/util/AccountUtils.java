066dbc67306de99f1451ac292c766192f4f95b7d
Luis Weich <weichluis.sw@gmail.com>
2022-01-29 00:17:08 +0100

    AccountUtils toUser

--- ewul-backend/ewul-core/src/main/java/org/ewul/core/util/AccountUtils.java
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/util/AccountUtils.java
@@ -1,10 +1,15 @@
 package org.ewul.core.util;
 
+import org.ewul.model.BasicUser;
+import org.ewul.model.User;
 import org.ewul.model.db.auth.Account;
 import org.ewul.model.db.auth.Group;
 import org.ewul.model.db.auth.Role;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public final class AccountUtils {
@@ -12,25 +17,31 @@ public final class AccountUtils {
     private AccountUtils() {
     }
 
-    public static Set<String> getRolesAsString(Account account) {
+    public static User toUser(Account account) {
 
         if (account == null) {
             throw new NullPointerException("account");
         }
 
-        Group group = account.getGroup();
-        if (group != null) {
-            Set<Role> roles = group.getRoles();
-            if (roles != null) {
-                Set<String> result = roles.stream()
-                        .filter(Objects::nonNull)
-                        .map(Role::getName)
-                        .collect(Collectors.toSet());
-                return Collections.unmodifiableSet(result);
-            }
-        }
+        BasicUser result = new BasicUser();
 
-        return Collections.emptySet();
+        result.setId(account.getId());
+        result.setName(account.getName());
+        result.setRoles(NullUtils.safeGet(() -> {
+            Group group = account.getGroup();
+            if (group == null) {
+                throw new NullPointerException("group");
+            }
+            Set<String> roles = group.getRoles()
+                    .stream()
+                    .filter(Objects::nonNull)
+                    .map(Role::getName)
+                    .collect(Collectors.toSet());
+            return Collections.unmodifiableSet(roles);
+        }, Collections.emptySet()));
+        result.setProperties(Collections.unmodifiableMap(new LinkedHashMap<>(account.getProperties())));
+
+        return result;
     }
 
 }
