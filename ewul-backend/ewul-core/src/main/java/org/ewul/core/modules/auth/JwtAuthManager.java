cb3087e269053352380390fed13f6fa283e8cb9a
Luis Weich <weichluis.sw@gmail.com>
2022-02-06 20:50:59 +0100

    add JwtAuthManager

---
+++ ewul-backend/ewul-core/src/main/java/org/ewul/core/modules/auth/JwtAuthManager.java
@@ -0,0 +1,47 @@
+package org.ewul.core.modules.auth;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import org.ewul.core.jwt.JwtDecoder;
+import org.ewul.model.User;
+import org.ewul.model.config.CoreConfiguration;
+import org.ewul.model.config.JwtConfiguration;
+
+import javax.inject.Inject;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+public class JwtAuthManager extends AbstractAuthManager {
+
+    private final JwtConfiguration jwtConfiguration;
+    private final Algorithm algorithm;
+
+    public JwtAuthManager(JwtConfiguration jwtConfiguration, Algorithm algorithm) {
+        this.jwtConfiguration = Objects.requireNonNull(jwtConfiguration);
+        this.algorithm = Objects.requireNonNull(algorithm);
+    }
+
+    public JwtAuthManager(JwtConfiguration jwtConfiguration) {
+        this.jwtConfiguration = Objects.requireNonNull(jwtConfiguration);
+        this.algorithm = buildAlgorithm();
+    }
+
+    @Inject
+    public JwtAuthManager(CoreConfiguration configuration) {
+        this(configuration.getJwtConfiguration());
+    }
+
+    private Algorithm buildAlgorithm() {
+        String secret = jwtConfiguration.getSecret();
+        if (secret == null) {
+            secret = UUID.randomUUID().toString();
+        }
+        return Algorithm.HMAC512(secret);
+    }
+
+    @Override
+    public Optional<User> decodeAccessToken(String accessToken) {
+        return new JwtDecoder(algorithm).decodeAccessToken(accessToken);
+    }
+
+}
